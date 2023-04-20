0514d267e621636728147dec0ae4a2d24ab45da3
femnn <2362257850@qq.com>
2023-04-20 23:09:36 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navToggle = document.querySelector(".nav-toggle");
+    const navMenu = document.querySelector(".nav-menu");
+    const body = document.querySelector("body");
+
+    navToggle.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
+
+    body.addEventListener("click", function (event) {
+        if (!navToggle.contains(event.target) && !navMenu.contains(event.target)) {
+            navMenu.classList.remove("active");
+        }
+    });
+});
 
